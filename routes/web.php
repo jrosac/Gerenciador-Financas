9e9698e8d394caed0d3dc4d3817e00869e95acda
joao <jrosac2002@academico.ufs.br>
2025-09-16 10:45:25 -0300

    feat: adicionada novas rotas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//-----------------------------------------------------------------
+
 Route::get('/login', function () {
     return view('credenciamento/login');
 })->name("login");
@@ -13,3 +15,19 @@ Route::get('/login', function () {
 Route::get('/cadastro', function () {
     return view('credenciamento/cadastro');
 })->name('cadastro');
+
+//-----------------------------------------------------------------
+
+Route::get('/home', function () {
+    return view('usuario/home');
+})->name('usuario');
+
+Route::get('/update', function () {
+    return view('usuario/update');
+})->name('update');
+
+Route::get('/createCompra',function (){
+    return view('usuario/createCompra');
+})->name("createCompra");
+
+
