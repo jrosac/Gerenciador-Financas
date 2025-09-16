7a339dcac7c3c85f5b2d76270b8d7f5e5873a68e
joao <jrosac2002@academico.ufs.br>
2025-09-16 10:43:40 -0300

    feat: adicionei o link da página de welcome na logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,8 +11,8 @@
     <div class="min-h-screen h-full bg-gradient-to-b bg-gray-900 to-green-900">
 
         <header class="bg-black/70 backdrop-blur-md text-white flex justify-between sticky top-0 items-center m-0 p-1">
-            <img src="{{Vite::asset('resources/assets/logo4.png')}}" alt="Logo da empresa" class="w-22 h-18 m-0 p-0" >
-        <ul class="flex gap-6 ">
+            <a href="/"><img src="{{Vite::asset('resources/assets/logo4.png')}}" alt="Logo da empresa" class="w-22 h-18 m-0 p-0" ></a>
+        <ul class="flex gap-6 mr-4">
           <li><a href="#" class="hover:text-green-300 text-lg">Home</a></li>
           <li><a href="#" class="hover:text-green-300 text-lg">Sobre</a></li>
           <li><a href="#" class="hover:text-green-300 text-lg">Contato</a></li>
