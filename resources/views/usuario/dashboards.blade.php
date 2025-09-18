ddb226f34c9d025c1249e490076ab11682c17541
joao <jrosac2002@academico.ufs.br>
2025-09-18 09:21:07 -0300

    feat: adicionei os dashboards estaticos no template

--- resources/views/usuario/dashboards.blade.php
+++ resources/views/usuario/dashboards.blade.php
@@ -1,31 +1,83 @@
 @extends('layouts.admin')
 
 @section('content')
+
+
 @php
 use ArielMejiaDev\LarapexCharts\LarapexChart;
 
+
+
 // Gráfico de pizza estático
-$chart = (new LarapexChart)->pieChart()
+$chartPizza= (new LarapexChart)->pieChart()
     ->setTitle('Compras por Status')
     ->setSubtitle('Resumo do sistema')
     ->addData([5, 8, 2])
-    ->setLabels(['Baixa', 'Regular', 'Elevada'])
-    ->setColors(['#22c55e', '#facc15', '#ef4444'])
-    ->setFontColor('#ff6384');
+    ->setLabels(['Baixa', 'Regular', 'Elevada']);
+
+$chartBarra = (new ArielMejiaDev\LarapexCharts\LarapexChart)->barChart()
+     ->setTitle('Gastos - 2025')
+     ->setSubtitle('Valores em reais')
+     ->addData('Vendas', [120.00, 90.00, 150.00, 200.00, 170.00, 140.00, 220.00, 180.00, 160.00, 190.00, 210.00, 250.00])
+     ->setXAxis([
+         'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+         'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+     ])
+     ->setColors(['#10B981'])
+     ->setDataLabels(true)
+     ->setHeight(500)
+     ->setGrid();
+
+$chartLinha = (new LarapexChart)->lineChart()
+    ->setTitle('Vendas Mensais - 2025')
+    ->setSubtitle('Valores em unidades')
+    ->addData('Vendas', [120, 90, 150, 200, 170, 140, 220, 180, 160, 190, 210, 250])
+    ->setXAxis([
+        'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+    ])
+    ->setColors(['#10B981']) // cor da linha
+    ->setDataLabels(true)
+    ->setHeight(400)
+    ->setGrid();
+
+
 
 @endphp
 
-<div class="min-h-screen p-6 bg-gray-900 text-white">
-    <h1 class="text-3xl font-bold mb-6 text-center">Dashboards Estático</h1>
+<h1 class="text-4xl font-bold mt-10 mb-15 text-center text-white">
+    Dashboards
+</h1>
 
-    <div class="max-w-md mx-auto ">
-        {!! $chart->container() !!}
+<div class="flex col">
+
+<div class="flex min-h-screen gap-6 px-6">
+    <div class="w-1/2 pr-10">
+        {!! $chartPizza->container() !!}
+    </div>
+
+    <div class="w-1/2 pl-10">
+        {!! $chartBarra->container() !!}
+    </div>
+</div>
+
+    <div>
+        {!! $chartLinha->container() !!}
     </div>
 
 
-    {!! $chart->script() !!}
+</div>
+
+
+
+{!! $chartPizza->script() !!}
+{!! $chartBarra->script() !!}
+{!! $chartLinha->script() !!}
+
 </div>
 @endsection
+
+
 
 
 
