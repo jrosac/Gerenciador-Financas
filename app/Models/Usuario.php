f5af9af34af53945fc5c31476a46c016a99b1b08
joao <jrosac2002@academico.ufs.br>
2025-09-19 10:12:51 -0300

    feat: adicionada as models e seus métodos de relacionameto

---
+++ app/Models/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $table = 'usuarios';
+
+    protected $guarded = [ 'id' ];
+
+    function compras()
+    {
+        return $this->hasMany(Compra::class, 'usuario_id');
+    }
+}
