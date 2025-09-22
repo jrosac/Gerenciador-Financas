d72c4af0d61125e4fe7915489eedb18e08160ee8
joao <jrosac2002@academico.ufs.br>
2025-09-22 09:10:09 -0300

    feat: criada a controller de autenticação do usuário

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function index(){
+        return view('credenciamento/login');
+    }
+
+    public function loginAttempt(Request $request){
+
+    $request->validate([
+        'email' => 'required|email',
+        'senha' => 'required|string',
+    ]);
+
+    // mapeia 'senha' para 'password'
+    $credentials = [
+        'email' => $request->email,
+        'password' => $request->senha,
+    ];
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->with('status', 'Credenciais inválidas!');
+
+}
+
+
+}
+
