80023d0b8057affbaa542d442fd15818904323b8
joao <jrosac2002@academico.ufs.br>
2025-09-22 10:03:02 -0300

    feat: as rotas principais foram colocadas dentro de um middleware para serem acessadas somente após autenticação

--- routes/web.php
+++ routes/web.php
@@ -10,40 +10,44 @@ Route::get('/', function () {
 
 //-----------------------------------------------------------------
 
-Route::get('/login',[AuthController::class,'index'])->name('login');
-
-Route::post('/login',[AuthController::class,'loginAttempt'])->name('auth');
 
 Route::get('/cadastro',[UsuarioController::class,'create'])->name('cadastro');
 
 Route::post('/cadastro',[UsuarioController::class,'store'])->name('cadastro.store');
 
+Route::get('/login',[AuthController::class,'index'])->name('login');
+
+Route::post('/login',[AuthController::class,'loginAttempt'])->name('auth');
+
+Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
+
+
+
 //-----------------------------------------------------------------
 
-Route::get('/home', function () {
-    return 'Logado com sucesso!';
-})->name('home');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home', function () {
+        return view('usuario/home');
+    })->name('home');
+
+    Route::get('/update', function () {
+        return view('usuario/update');
+    })->name('update');
+
+    Route::get('/createCompra', function () {
+        return view('usuario/createCompra');
+    })->name("createCompra");
+
+    Route::get('/indexCompra', function () {
+        return view('usuario/indexCompra');
+    })->name("IndexCompra");
+
+    Route::get('/dashboards', function () {
+        return view('usuario/dashboards');
+    })->name("dashboards");
 
 
-// Route::get('/home', function () {
-//     return view('usuario/home');
-// })->name('usuario');
-
-Route::get('/update', function () {
-    return view('usuario/update');
-})->name('update');
-
-
-
-Route::get('/createCompra',function (){
-    return view('usuario/createCompra');
-})->name("createCompra");
-
-Route::get('/indexCompra',function (){
-    return view('usuario/indexCompra');
-})->name("IndexCompra");
-
-Route::get('/dashboards',function (){
-    return view('usuario/dashboards');
-})->name("dashboards");
+});
 
