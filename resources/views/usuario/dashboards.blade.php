270ab233a98bb3357ad5c100c91595a82eb2b262
joao <jrosac2002@academico.ufs.br>
2025-09-23 11:25:26 -0300

    feat: adicionei o gráfico de pizza dinâmico

--- resources/views/usuario/dashboards.blade.php
+++ resources/views/usuario/dashboards.blade.php
@@ -3,47 +3,7 @@
 @section('content')
 
 
-@php
-use ArielMejiaDev\LarapexCharts\LarapexChart;
 
-
-
-// Gráfico de pizza estático
-$chartPizza= (new LarapexChart)->pieChart()
-    ->setTitle('Compras por Status')
-    ->setSubtitle('Resumo do sistema')
-    ->addData([5, 8, 2])
-    ->setLabels(['Baixa', 'Regular', 'Elevada']);
-
-$chartBarra = (new ArielMejiaDev\LarapexCharts\LarapexChart)->barChart()
-     ->setTitle('Gastos - 2025')
-     ->setSubtitle('Valores em reais')
-     ->addData('Vendas', [120.00, 90.00, 150.00, 200.00, 170.00, 140.00, 220.00, 180.00, 160.00, 190.00, 210.00, 250.00])
-     ->setXAxis([
-         'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
-         'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
-     ])
-     ->setColors(['#10B981'])
-     ->setDataLabels(true)
-     ->setHeight(500)
-     ->setGrid();
-
-$chartLinha = (new LarapexChart)->lineChart()
-    ->setTitle('Gastos Mensais - 2025')
-    ->setSubtitle('Valores em unidades')
-    ->addData('Vendas', [120, 90, 150, 200, 170, 140, 220, 180, 160, 190, 210, 250])
-    ->setXAxis([
-        'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
-        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
-    ])
-    ->setColors(['#10B981']) // cor da linha
-    ->setDataLabels(true)
-    ->setHeight(400)
-    ->setGrid();
-
-
-
-@endphp
 
 <h1 class="text-4xl font-bold mt-10 mb-15 text-center text-white">
     Dashboards
@@ -54,7 +14,7 @@ $chartLinha = (new LarapexChart)->lineChart()
     <!-- Primeira linha com 2 gráficos lado a lado -->
     <div class="flex gap-6 mb-10">
         <div class="w-1/2 px-10">
-            {!! $chartPizza->container() !!}
+            {{-- {!! $chartPizza->container() !!} --}}
         </div>
 
         <div class="w-1/2 px-10">
@@ -64,7 +24,7 @@ $chartLinha = (new LarapexChart)->lineChart()
 
     <!-- Linha de baixo com gráfico ocupando 100% -->
     <div class="w-full">
-        {!! $chartLinha->container() !!}
+        {{-- {!! $chartLinha->container() !!} --}}
     </div>
 
 </div>
@@ -72,9 +32,8 @@ $chartLinha = (new LarapexChart)->lineChart()
 
 
 
-{!! $chartPizza->script() !!}
 {!! $chartBarra->script() !!}
-{!! $chartLinha->script() !!}
+
 
 </div>
 @endsection
