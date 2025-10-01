757c86ab6a27d4dd79c47c83a8d4f683fc02c580
joao <jrosac2002@academico.ufs.br>
2025-10-01 08:52:55 -0300

    feat: adicionada uma chave de sucesso para mostrar mensagem na view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
 
-            return redirect()->intended('home');
+            return redirect()->intended('home')->with('success', 'Login realizado com sucesso!');
         }
 
         return back()->withInput()->with('status', 'Credenciais inválidas!');
